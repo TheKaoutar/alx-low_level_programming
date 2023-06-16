1edfa0b3e6cbf48a8904b77799fce0005bc6c183
TheKaoutar <kaoutar.bakri7@gmail.com>
2023-06-15 19:22:35 -0700

    my first C commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : A C program that prints with puts function
+ *
+ * Return : Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
